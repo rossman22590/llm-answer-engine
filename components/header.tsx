72a854834f662834b4c1737714938fcde041fd32
Ross Cohen <rosscohen1990@gmail.com>
2024-04-13 01:19:51 -0400

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -15,8 +15,8 @@ export function Header() {
     <>
       <header className="sticky top-0 z-50 flex items-center justify-between w-full px-4 border-b h-14 shrink-0 dark:bg-slate-800 bg-white backdrop-blur-xl">
         <span className="inline-flex items-center home-links whitespace-nowrap">
-          <a href="https://developersdigest.tech" rel="noopener" target="_blank">
-            <span className="block sm:inline text-lg sm:text-xl lg:text-2xl font-semibold dark:text-white text-black">answer <span className="linear-wipe">engine</span></span>
+          <a href="https://myapps.ai" rel="noopener" target="_blank">
+            <span className="block sm:inline text-lg sm:text-xl lg:text-2xl font-semibold dark:text-white text-black">Datavibes <span className="linear-wipe">Pro</span></span>
           </a>
         </span>
         <div className="flex items-center justify-end space-x-2">
